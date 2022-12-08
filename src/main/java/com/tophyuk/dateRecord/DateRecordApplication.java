9d9573849cb2f4562c68693e233ff8da10190bff
상혁 <sanghyuk1992@naver.com>
2022-12-08 23:42:05 +0900

    add : 환경세팅

---
+++ src/main/java/com/tophyuk/dateRecord/DateRecordApplication.java
@@ -0,0 +1,13 @@
+package com.tophyuk.dateRecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tophyuk/dateRecord/DateRecordApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tophyuk.dateRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
