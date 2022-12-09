fd8dc5d462a38deb27f0fb2c61d969513ad8921e
상혁 <sanghyuk1992@naver.com>
2022-12-10 00:13:21 +0900

    feat : User domain 지역 추가

--- src/main/java/com/tophyuk/dateRecord/domain/User.java
+++ src/main/java/com/tophyuk/dateRecord/domain/User.java
@@ -4,12 +4,14 @@ package com.tophyuk.dateRecord.domain;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
 @Entity
 public class User {
 
@@ -17,16 +19,19 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length=10, nullable = false)
+    @Column(length = 10, nullable = false)
     private String name; // 이름
 
-    @Column(length=20, nullable = false)
+    @Column(length = 20, nullable = false)
     private String email; // 이메일
 
-    @Column(length=400, nullable = false)
+    @Column(length = 10, nullable = false)
+    private String regions; // 지역
+
+    @Column(length = 400, nullable = false)
     private String password; // 패스워드
 
-    @Column(length=400, nullable = false)
+    @Column(length = 400, nullable = false)
     private String salt; // 암호화
 
     @CreatedDate
@@ -35,13 +40,11 @@ public class User {
     private LocalDateTime updatedAt; // 업데이트된 시간
 
     @Builder
-    public User(Long id, String name, String email, String password, String salt, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public User(Long id, String name, String email, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
-        this.salt = salt;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
+
     }
 }
