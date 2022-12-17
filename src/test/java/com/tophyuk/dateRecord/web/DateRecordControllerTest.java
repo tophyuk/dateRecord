940a75614c616fb226bc8a8b3e703204c5df16ca
상혁 <sanghyuk1992@naver.com>
2022-12-17 19:02:51 +0900

    test : 이메일 중복 체크 추가

--- src/test/java/com/tophyuk/dateRecord/web/DateRecordControllerTest.java
+++ src/test/java/com/tophyuk/dateRecord/web/DateRecordControllerTest.java
@@ -4,16 +4,15 @@ import com.tophyuk.dateRecord.domain.User;
 import com.tophyuk.dateRecord.repository.UserRepository;
 import com.tophyuk.dateRecord.service.UserService;
 import com.tophyuk.dateRecord.validation.form.SignupForm;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class DateRecordControllerTest {
@@ -27,7 +26,7 @@ public class DateRecordControllerTest {
 
         //given
         String name ="상혁";
-        String email = "sanghyuk199@naver.com";
+        String email = "sanghyuk1992@naver.com";
         String region = "DAEGU";
         String password ="wjdtkdgur";
 
@@ -38,11 +37,22 @@ public class DateRecordControllerTest {
         form.setRegion(region);
         form.setPassword(password);
 
+        //회원가입
         userService.save(form);
 
         //then
         //User resultUser = userRepository.findByEmail(email);
         //Assertions.assertThat(resultUser.getEmail()).isEqualTo(user.getEmail());
+
+    }
+
+    @Test
+    void emailOverlapCheck() {
+
+        String email ="sanghyuk992@naver.com";
+
+        Optional<User> byEmail = userService.findByEmail(email);
+        assertTrue(byEmail.isPresent());
 
     }
 }
